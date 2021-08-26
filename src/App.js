20326a9b04fd37c7310740b48b55f0d2b61ff2fe
Gary Bunofsky <gary@garybunofsky.com>
2021-08-26 09:28:11 -0400

    adjusted some styles

--- src/App.js
+++ src/App.js
@@ -24,8 +24,7 @@ class App extends React.Component {
       region.isSelected = region.value === selected;
       return region;
     });
-    debugger;
-    
+
     this.setState({ 
       regions: regions,
       questionsToShow: 1,
@@ -41,7 +40,7 @@ class App extends React.Component {
     let questionValue = e.target.name;
     let clickedQuestion = questionsCopy.filter(question => question.value === questionValue)[0]
     let prevQuestionChanged = clickedQuestion.id < this.state.questionsToShow;
-    
+
     if (prevQuestionChanged) {
       this.clearProceedingQuestions(clickedQuestion.id);
     }
@@ -100,21 +99,47 @@ class App extends React.Component {
 
   render() {
     const { regions, questions, result, questionsToShow, canVote} = this.state;
-    let resultHeading = (typeof canVote != "boolean") ? 'You might be able to vote...' : canVote ? 'You can vote!' : 'You can\'t vote.';
-    let resultClass = (typeof canVote != "boolean") ? 'result-wrapper-blue' : canVote ? 'result-wrapper-green' : 'result-wrapper-red';
+    let noAnswer = typeof canVote != "boolean";
+    let resultHeading = noAnswer ?
+      'You might be able to vote...' : canVote ?
+      'You can vote!' : 'You can\'t vote.';
+    let resultClass = noAnswer ?
+      'result-wrapper-blue' : canVote ?
+      'result-wrapper-green' : 'result-wrapper-red';
 
+    const selectStyles = {
+      control: (base, state) => ({
+        ...base,
+        height: '46px',
+        'min-height': '46px',
+      }),
+    };
     let regionField = (
       <div className="form-group">
         <label>State</label>
-        <Select options={regions} name="regions" id="regions" onChange={(e) => this.handleRegionChange(e)} />
+        <Select
+          styles={selectStyles}
+          options={regions}
+          name="regions"
+          id="regions"
+          onChange={(e) => this.handleRegionChange(e)}
+          theme={theme => ({
+            ...theme,
+            borderRadius: 0,
+            colors: {
+              ...theme.colors,
+              primary25: '#f3f6ff',
+              primary: '#3466ff',
+            },
+          })}
+        />
       </div>
     );
 
-    let questionFields = (questions ? questions.map((question, i) => 
-      
+    let questionFields = (questions ? questions.map((question, i) =>
         <div className={(i < questionsToShow) ? 'form-group' : 'form-group hide'}>
           <label>{question.value}</label>
-          <p>{question.extra}</p>
+          {question.extra ? `<p>${question.extra}</p>` : null }
           {this.renderOptions(question)}
         </div>
 
@@ -133,12 +158,9 @@ class App extends React.Component {
       <div className="App">
         <h1>Can I vote?</h1>
         <h2 className="lead">Voting laws are pretty complex. Check if you are eligible to vote in your state today. Just complete the form below.</h2>
-        
         {regionField}
         {questionFields}
-
         {result && resultWrapper}
-        
       </div>
     );
   }
