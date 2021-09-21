bdf209c8b416556d4bc23b3d4500279c97f7d9bb
Gary Bunofsky <gary@garybunofsky.com>
2021-09-21 15:18:23 -0400

    Added link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import './App.css';
-import Select from 'react-select';
-import data from './data.json';
+import React from "react";
+import "./App.css";
+import Select from "react-select";
+import data from "./data.json";
 
 class App extends React.Component {
   constructor(props) {
@@ -20,10 +20,10 @@ class App extends React.Component {
     };
   }
 
-  handleRegionChange = (e) => {
+  handleRegionChange = e => {
     let regionCopy = data.slice();
     let selected = e.value;
-    let prevSelectedRegion = regionCopy.filter(region => region.isSelected)[0]
+    let prevSelectedRegion = regionCopy.filter(region => region.isSelected)[0];
 
     let regions = regionCopy.map(region => {
       region.isSelected = region.value === selected;
@@ -32,25 +32,28 @@ class App extends React.Component {
 
     let selectedRegion = this.selectedRegion();
     let noQuestions = selectedRegion.questions === null;
-    let regionHasntChanged = prevSelectedRegion && selected === prevSelectedRegion.label;
+    let regionHasntChanged =
+      prevSelectedRegion && selected === prevSelectedRegion.label;
 
     if (regionHasntChanged) return;
 
-    this.setState({ 
+    this.setState({
       regions: regions,
       questionsToShow: 1,
       questions: selectedRegion.questions,
       result: noQuestions ? selectedRegion.result : null,
       canVote: noQuestions ? true : null
-    })
-  }
+    });
+  };
 
-  handleQuestionChange = (e) => {
+  handleQuestionChange = e => {
     var stateCopy = Object.assign({}, this.state);
     let questionsCopy = stateCopy.questions.slice();
     let selected = e.target.value;
     let questionValue = e.target.name;
-    let clickedQuestion = questionsCopy.filter(question => question.value === questionValue)[0]
+    let clickedQuestion = questionsCopy.filter(
+      question => question.value === questionValue
+    )[0];
     let prevQuestionChanged = clickedQuestion.id < this.state.questionsToShow;
 
     if (prevQuestionChanged) {
@@ -64,18 +67,23 @@ class App extends React.Component {
       return question;
     });
 
-    let selectedOption = clickedQuestion.options.filter(option => option.value === selected)[0]
+    let selectedOption = clickedQuestion.options.filter(
+      option => option.value === selected
+    )[0];
     let increment = selectedOption.result ? 0 : 1;
-    let questionsToShow = questions.filter(questions => questions.selectedOption).length + increment;
+    let questionsToShow =
+      questions.filter(questions => questions.selectedOption).length +
+      increment;
 
-    this.setState({ questions: questions,
+    this.setState({
+      questions: questions,
       questionsToShow: questionsToShow,
-      result: selectedOption.result, 
+      result: selectedOption.result,
       canVote: selectedOption.canVote
-    })
-  }
+    });
+  };
 
-  clearProceedingQuestions = (id) => {
+  clearProceedingQuestions = id => {
     var stateCopy = Object.assign({}, this.state);
     let questionsCopy = stateCopy.questions.slice();
 
@@ -83,49 +91,55 @@ class App extends React.Component {
       if (id < question.id) {
         question.selectedOption = null;
       }
-      return question
-    })
-    this.setState({ questions: questionsCopy })
-  }
+      return question;
+    });
+    this.setState({ questions: questionsCopy });
+  };
 
-  renderOptions = (question) => (
-    question.options ? 
-      (question.options.map(option => (
-        <span className="radio-wrapper">
-          <input 
-            type="radio" 
-            className="btn-check"
-            key={option.value}
-            name={question.value}
-            value={option.value}
-            checked={question.selectedOption === option.value}
-            onChange={(e) => this.handleQuestionChange(e)}
-          />
-          <label className="btn" htmlFor={question.value}>{option.value}</label>
-        </span>
-      )))
-      : null
-  )
+  renderOptions = question =>
+    question.options
+      ? question.options.map(option => (
+          <span className="radio-wrapper">
+            <input
+              type="radio"
+              className="btn-check"
+              key={option.value}
+              name={question.value}
+              value={option.value}
+              checked={question.selectedOption === option.value}
+              onChange={e => this.handleQuestionChange(e)}
+            />
+            <label className="btn" htmlFor={question.value}>
+              {option.value}
+            </label>
+          </span>
+        ))
+      : null;
 
-  selectedRegion = () => this.state.regions.filter(region => region.isSelected)[0]
+  selectedRegion = () =>
+    this.state.regions.filter(region => region.isSelected)[0];
 
   render() {
-    const { regions, questions, result, questionsToShow, canVote} = this.state;
+    const { regions, questions, result, questionsToShow, canVote } = this.state;
     let noAnswer = typeof canVote != "boolean";
-    let resultHeading = noAnswer ?
-      'You might be able to vote...' : canVote ?
-      'You can vote!' : 'You can\'t vote.';
+    let resultHeading = noAnswer
+      ? "You might be able to vote..."
+      : canVote
+        ? "You can vote!"
+        : "You can't vote.";
 
-    let resultClass = noAnswer ?
-      'result-wrapper-blue' : canVote ?
-      'result-wrapper-green' : 'result-wrapper-red';
+    let resultClass = noAnswer
+      ? "result-wrapper-blue"
+      : canVote
+        ? "result-wrapper-green"
+        : "result-wrapper-red";
 
     const selectStyles = {
       control: (base, state) => ({
         ...base,
-        height: '46px',
-        minHeight: '46px',
-      }),
+        height: "46px",
+        minHeight: "46px"
+      })
     };
 
     let regionField = (
@@ -133,41 +147,53 @@ class App extends React.Component {
         <label>Where do you live?</label>
         <Select
           styles={selectStyles}
-          classNamePrefix='reactSelect'
+          classNamePrefix="reactSelect"
           options={regions}
           name="regions"
           id="regions"
-          onChange={(e) => this.handleRegionChange(e)}
+          onChange={e => this.handleRegionChange(e)}
           theme={theme => ({
             ...theme,
             borderRadius: 0,
             colors: {
               ...theme.colors
-            },
+            }
           })}
         />
-        <p class="small">Data provided by Hip Hop Caucus' Respect My Vote! campaign.</p>
+        <p class="small">
+          Data provided by Hip Hop Caucus'{" "}
+          <a href="https://respectmyvote.com" target="_blank">
+            Respect My Vote!
+          </a>{" "}
+          campaign.
+        </p>
       </div>
     );
 
-    let questionFields = (questions ? questions.map((question, i) =>
-      <div className={(i < questionsToShow) ? 'form-group' : 'form-group hide'}>
-        <label>{question.value}</label>
-        <p dangerouslySetInnerHTML={{__html: question.extra}}></p>
-        {this.renderOptions(question)}
-      </div>
-    ) : null);
+    let questionFields = questions
+      ? questions.map((question, i) => (
+          <div
+            className={i < questionsToShow ? "form-group" : "form-group hide"}
+          >
+            <label>{question.value}</label>
+            <p dangerouslySetInnerHTML={{ __html: question.extra }} />
+            {this.renderOptions(question)}
+          </div>
+        ))
+      : null;
 
-    let resultWrapper = (<div className={`result-wrapper ${resultClass}`} role="alert">
-      <div className="result-text">
-        <span>
-          <span className="row">
-            <h2>{resultHeading}</h2>
+    let resultWrapper = (
+      <div className={`result-wrapper ${resultClass}`} role="alert">
+        <div className="result-text">
+          <span>
+            <span className="row">
+              <h2>{resultHeading}</h2>
+            </span>
+            <p dangerouslySetInnerHTML={{ __html: result }} />
           </span>
-          <p dangerouslySetInnerHTML={{__html: result}}></p>
-        </span>
+        </div>
       </div>
-    </div>)
+    );
 
     return (
       <div className={`App ${this.state.src}`}>
