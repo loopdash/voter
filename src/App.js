8024d20cdaab7f62efa2b10f54567d0bc662ed34
Gary Bunofsky <gary@garybunofsky.com>
2022-04-07 14:05:46 -0400

    Removed unused link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from './Home';
 import About from './About';
