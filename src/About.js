7bb2f280519167117b57c48c5d0e343101f820c7
Gary Bunofsky <gary@garybunofsky.com>
2022-04-07 14:00:53 -0400

    Added about page

---
+++ src/About.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './App.css';
+
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className={`About`}>
+        <p className="small"><Link to="/">Back to questionnaire</Link></p>
+        <h1>Duo Reges: constructio interrete.</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vide, quantum, inquam, fallare, Torquate. Teneo, inquit, finem illi videri nihil dolere. Quantum Aristoxeni ingenium consumptum videmus in musicis? Et nemo nimium beatus est; Hic ambiguo ludimur. At iam decimum annum in spelunca iacet. </p>
+        <p>Haec para/doca illi, nos admirabilia dicamus. Sed nimis multa. Itaque contra est, ac dicitis; </p>
+        <h2>Duo Reges: constructio interrete.</h2>
+        <p>
+          Ultrices tortor nullam tristique augue dolor varius! Libero aliquet curae; dolor. Himenaeos posuere elit nec aptent metus cras mi ultricies at elit arcu? Consectetur lorem suspendisse ornare vel sit nulla massa dapibus in, eget sagittis suscipit. Id magna vestibulum mi? Malesuada donec semper hendrerit. Curae; ullamcorper proin facilisi. Condimentum leo tortor cras tortor nisi sem suscipit duis? Ante suscipit, viverra commodo nulla semper consectetur eros. Sit at nullam amet, dis convallis condimentum? Viverra hac aliquet morbi in a dui cubilia cum auctor volutpat.
+        </p>
+
+        <img src="https://images.unsplash.com/photo-1540908390241-82158ab62887?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1769&q=80" />
+        <h3>Volutpat ut pretium</h3>
+        <p>
+          Purus fusce aptent placerat odio penatibus mus. Vel malesuada sociis diam molestie nullam lobortis. Convallis, tortor a urna? Aliquet litora ante consequat? Enim imperdiet est faucibus quisque metus vehicula at mauris. Hendrerit tempor vehicula enim conubia sociis. Magnis felis condimentum ligula rhoncus, commodo lobortis. Tempor nascetur suscipit proin enim. Imperdiet turpis ut morbi consequat. Integer, phasellus himenaeos quis egestas nisl orci montes.
+        </p>
+        <h3>Mus at nibh faucibus</h3>
+        <p>
+          Magnis imperdiet torquent commodo ultrices cubilia condimentum penatibus urna. Mus at nibh faucibus sociis feugiat ac diam volutpat aliquam! Quis in venenatis magna curabitur? Aenean pellentesque mattis molestie? Aptent diam interdum nec viverra tristique nam vestibulum arcu fermentum donec etiam. Arcu tempus scelerisque magna eu taciti diam. Semper magnis ornare fringilla nullam vehicula hendrerit pharetra nec faucibus cras. Parturient fusce nam posuere fusce penatibus fusce nunc proin leo ornare. Etiam tempor amet torquent sollicitudin viverra nunc nulla convallis vulputate morbi consequat. Dictumst vel hendrerit etiam.
+        </p>
+        <h4>Ipsum orci</h4>
+        <p>
+          Varius mattis nibh faucibus tempus diam placerat aliquam. Ipsum orci dignissim augue convallis cubilia platea consequat nisl tempor lacus. Mollis ultrices ullamcorper molestie vel sociis pellentesque. Tortor vehicula varius elit egestas, lectus ornare habitasse posuere. Sapien feugiat in nam aliquet urna. Dis fames quam dis! Pharetra quam quis tortor consectetur. Sociis vitae eu hendrerit duis taciti! Natoque convallis sodales purus hac vitae dignissim. Litora, sit massa maecenas ad aptent magnis felis iaculis magna semper. Interdum.
+        </p>
+        <p>
+          Nascetur, arcu libero dui gravida dictum montes! Proin porttitor venenatis consequat netus phasellus odio accumsan ligula iaculis. Netus sociosqu aliquet leo ante cubilia ad aliquet lobortis ante elementum. Pharetra hendrerit ut sodales rhoncus. Vulputate tellus tortor est ullamcorper orci mi. Elit consectetur malesuada non placerat tempor. Parturient tristique consequat mattis nostra neque rhoncus laoreet imperdiet ante iaculis pretium. Congue aenean etiam sollicitudin volutpat. Fermentum senectus praesent accumsan ligula ut ipsum auctor. Nam dictum vitae dignissim. Litora litora torquent litora porttitor est aliquet id! Sodales imperdiet mi pellentesque sed commodo ullamcorper egestas id feugiat. Cursus montes fames magna. Rutrum.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,182 +1,22 @@
-import React from 'react';
-import './App.css';
-import Select from 'react-select';
-import data from './data.json';
+import * as React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Home from './Home';
+import About from './About';
+import Typography from 'typography';
+import theme from 'typography-theme-grand-view';
+const typography = new Typography(theme);
+typography.injectStyles()
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    const urlSearchParams = new URLSearchParams(window.location.search);
-    const params = Object.fromEntries(urlSearchParams.entries());
-
-    this.state = {
-      regions: data,
-      questions: null,
-      result: null,
-      canVote: null,
-      questionsToShow: 1,
-      src: params.src
-    };
-  }
-
-  handleRegionChange = (e) => {
-    let regionCopy = data.slice();
-    let selected = e.value;
-    let prevSelectedRegion = regionCopy.filter(region => region.isSelected)[0]
-
-    let regions = regionCopy.map(region => {
-      region.isSelected = region.value === selected;
-      return region;
-    });
-
-    let selectedRegion = this.selectedRegion();
-    let noQuestions = selectedRegion.questions === null;
-    let regionHasntChanged = prevSelectedRegion && selected === prevSelectedRegion.label;
-
-    if (regionHasntChanged) return;
-
-    this.setState({ 
-      regions: regions,
-      questionsToShow: 1,
-      questions: selectedRegion.questions,
-      result: noQuestions ? selectedRegion.result : null,
-      canVote: noQuestions ? true : null
-    })
-  }
-
-  handleQuestionChange = (e) => {
-    var stateCopy = Object.assign({}, this.state);
-    let questionsCopy = stateCopy.questions.slice();
-    let selected = e.target.value;
-    let questionValue = e.target.name;
-    let clickedQuestion = questionsCopy.filter(question => question.value === questionValue)[0]
-    let prevQuestionChanged = clickedQuestion.id < this.state.questionsToShow;
-
-    if (prevQuestionChanged) {
-      this.clearProceedingQuestions(clickedQuestion.id);
-    }
-
-    let questions = questionsCopy.map(question => {
-      if (question.value === questionValue) {
-        question.selectedOption = selected;
-      }
-      return question;
-    });
-
-    let selectedOption = clickedQuestion.options.filter(option => option.value === selected)[0]
-    let increment = selectedOption.result ? 0 : 1;
-    let questionsToShow = questions.filter(questions => questions.selectedOption).length + increment;
-
-    this.setState({ questions: questions,
-      questionsToShow: questionsToShow,
-      result: selectedOption.result, 
-      canVote: selectedOption.canVote
-    })
-  }
-
-  clearProceedingQuestions = (id) => {
-    var stateCopy = Object.assign({}, this.state);
-    let questionsCopy = stateCopy.questions.slice();
-
-    questionsCopy.map(question => {
-      if (id < question.id) {
-        question.selectedOption = null;
-      }
-      return question
-    })
-    this.setState({ questions: questionsCopy })
-  }
-
-  renderOptions = (question) => (
-    question.options ? 
-      (question.options.map(option => (
-        <span className="radio-wrapper">
-          <input 
-            type="radio" 
-            className="btn-check"
-            key={option.value}
-            name={question.value}
-            value={option.value}
-            checked={question.selectedOption === option.value}
-            onChange={(e) => this.handleQuestionChange(e)}
-          />
-          <label className="btn" htmlFor={question.value}>{option.value}</label>
-        </span>
-      )))
-      : null
-  )
-
-  selectedRegion = () => this.state.regions.filter(region => region.isSelected)[0]
-
-  render() {
-    const { regions, questions, result, questionsToShow, canVote} = this.state;
-    let noAnswer = typeof canVote != "boolean";
-    let resultHeading = noAnswer ?
-      'You might be able to vote...' : canVote ?
-      'You can vote!' : 'You can\'t vote.';
-
-    let resultClass = noAnswer ?
-      'result-wrapper-blue' : canVote ?
-      'result-wrapper-green' : 'result-wrapper-red';
-
-    const selectStyles = {
-      control: (base, state) => ({
-        ...base,
-        height: '46px',
-        minHeight: '46px',
-      }),
-    };
-
-    let regionField = (
-      <div className="form-group">
-        <label>Where do you live?</label>
-        <Select
-          styles={selectStyles}
-          classNamePrefix='reactSelect'
-          options={regions}
-          name="regions"
-          id="regions"
-          onChange={(e) => this.handleRegionChange(e)}
-          theme={theme => ({
-            ...theme,
-            borderRadius: 0,
-            colors: {
-              ...theme.colors
-            },
-          })}
-        />
-        <p class="small">Data provided by Hip Hop Caucus' <a href="http://respectmyvote.com" target="_blank" rel="noreferrer">Respect My Vote!</a> campaign.</p>
-      </div>
-    );
-
-    let questionFields = (questions ? questions.map((question, i) =>
-      <div className={(i < questionsToShow) ? 'form-group' : 'form-group hide'}>
-        <label>{question.value}</label>
-        <p dangerouslySetInnerHTML={{__html: question.extra}}></p>
-        {this.renderOptions(question)}
-      </div>
-    ) : null);
-
-    let resultWrapper = (<div className={`result-wrapper ${resultClass}`} role="alert">
-      <div className="result-text">
-        <span>
-          <span className="row">
-            <h2>{resultHeading}</h2>
-          </span>
-          <p dangerouslySetInnerHTML={{__html: result}}></p>
-        </span>
-      </div>
-    </div>)
-
-    return (
-      <div className={`App ${this.state.src}`}>
-        {regionField}
-        {questionFields}
-        {result && resultWrapper}
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
