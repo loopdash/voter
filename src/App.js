df0204f38aba23f5b9f378d9719e8694573af0ed
Gary Bunofsky <gary@garybunofsky.com>
2022-06-28 12:23:43 -0400

    Attempting to track GA using custom events

--- src/App.js
+++ src/App.js
@@ -5,13 +5,25 @@ import Home from './Home';
 import About from './About';
 import Typography from 'typography';
 import theme from 'typography-theme-grand-view';
+import ReactGA from "react-ga4";
 const typography = new Typography(theme);
 typography.injectStyles()
 
 function App() {
   let appClass = window === window.parent ? 'wrapper' : null;
+  var domainReferrer = (window === window.parent) ?
+            document.location.href :
+            document.referrer;
 
-  console.log(document.referrer);
+  ReactGA.initialize([{
+    trackingId: "G-7RH0TMQQ64"
+  }]);
+
+
+  ReactGA.event({
+    category: "domain_referrer",
+    action: domainReferrer
+  });
 
   return (
     <div className={`App ${appClass}`}>
